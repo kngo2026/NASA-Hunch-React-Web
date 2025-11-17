059f4d3759237942a967b9685415c85959b86c78
kngo2026 <148019881+kngo2026@users.noreply.github.com>
2025-11-17 13:02:17 -0500

    Changes

--- nasa-medical-cabinet/vite.config.ts
+++ nasa-medical-cabinet/vite.config.ts
@@ -3,7 +3,12 @@ import react from '@vitejs/plugin-react'
 
 export default defineConfig({
   plugins: [react()],
+  root: './',
   base: '/',
+  build: {
+    outDir: 'dist',
+    emptyOutDir: true
+  },
   server: {
     host: '0.0.0.0',
     port: 5173,
